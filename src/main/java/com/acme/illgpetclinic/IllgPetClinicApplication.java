4fd521da0bfa0578e3dfea40724301f0eba75827
Ivan Leiva <leivagarcia18@gmail.com>
2018-10-28 00:08:25 -0500

    Initial commit

---
+++ src/main/java/com/acme/illgpetclinic/IllgPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.acme.illgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllgPetClinicApplication.class, args);
+	}
+}
